5429c3127197c8be8719aeea43c2a9d04829514a
curcuz <czkzcz.cmc@gmail.com>
2018-02-14 21:32:21 +0000

    actually run the python scripts

--- tx/start.sh
+++ tx/start.sh
@@ -1,9 +1,6 @@
 # Bring CAN bus 0 up
 /sbin/ip link set can0 up type can bitrate 500000
 
-# Bring CAN bus 1 up
-/sbin/ip link set can1 up type can bitrate 500000
-
 while [[ true ]]; do
   sleep 5
 done
